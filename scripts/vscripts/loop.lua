289e387d0d51ef482360d9c61e7983d5047d9c24
Pugik <pugik9023@gmail.com>
2023-07-07 22:26:53 +0300

    Create loop.lua

---
+++ scripts/vscripts/loop.lua
@@ -0,0 +1,11 @@
+-- in gamemode.lua 
+function barebones:InitGameMode()
+{
+    GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 0 )
+}
+
+-- in addon_game_mode.lua
+
+function barebones:OnThink()
+    print('baj baj')
+end
