04a2d62768ca36bf54bd3cb2d175a93fcf5a9bff
Pugik <pugik9023@gmail.com>
2023-07-15 23:15:41 +0300

    cleaning

--- courierother/scripts/vscripts/addon_game_mode.lua
+++
@@ -1,9 +0,0 @@
-function testgamemode:OnNPCSpawned(keys)
-	local entity = EntIndexToHScript(keys.entindex)
-	
-	if entity:GetUnitName() == "npc_dota_courier" and entity.bFirstSpawned == nil then		-- Если юнит - курьер, то заходим.
-		entity.bFirstSpawned = true
-		entity:AddNewModifier(entity, nil, "modifier_courier_untouchable", nil)
-		print("Выдаем модификатор.")
-	end
-end
