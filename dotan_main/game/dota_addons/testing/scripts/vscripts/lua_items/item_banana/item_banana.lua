c03c0952024da81cd21a35166f92394d49949e03
Pugik <pugik9023@gmail.com>
2023-07-19 18:35:52 +0300

    micro patch
    Strengthen neutral items and reduce their cost.
    Banana boost, now gives 50 mana as well.
    Fixed active heroes list.

--- dotan_main/game/dota_addons/testing/scripts/vscripts/lua_items/item_banana/item_banana.lua
+++ dotan_main/game/dota_addons/testing/scripts/vscripts/lua_items/item_banana/item_banana.lua
@@ -6,12 +6,14 @@ function item_banana:OnSpellStart()
     if IsServer() then
 		self.peel_duration = self:GetSpecialValueFor( "peel_duration" )
         self.health = self:GetSpecialValueFor( "health" )
+        self.mana = self:GetSpecialValueFor( "mana" )
 	end
 	
 	local caster = self:GetCaster()
 	
     if caster ~= nil then
         caster:SetHealth(caster:GetHealth() + self.health)
+        caster:SetMana(caster:GetMana() + self.mana)
 
         self.banana_peel = CreateUnitByName("npc_dotan_banana_peel", caster:GetAbsOrigin(), true, caster, caster:GetOwner(), caster:GetTeam())
         self.banana_peel:AddNewModifier(nil, nil, "modifier_kill", {duration = self.peel_duration})
