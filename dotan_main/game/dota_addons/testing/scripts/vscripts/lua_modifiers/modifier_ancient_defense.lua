b3e9db2c1f88855eb12342e0f3ccd847bfbc309f
Pugik <pugik9023@gmail.com>
2023-07-28 22:16:48 +0300

    big fix

--- dotan_main/game/dota_addons/testing/scripts/vscripts/lua_modifiers/modifier_ancient_defense.lua
+++ dotan_main/game/dota_addons/testing/scripts/vscripts/lua_modifiers/modifier_ancient_defense.lua
@@ -1,21 +1,10 @@
 modifier_ancient_defense = class({})
 LinkLuaModifier( "modifier_ancient_defense_bonus", "lua_modifiers/modifier_ancient_defense", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_ancient_defense_effect", "lua_modifiers/modifier_ancient_defense", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_ancient_defense_effect_hero", "lua_modifiers/modifier_ancient_defense", LUA_MODIFIER_MOTION_NONE )
 
 function modifier_ancient_defense:IsHidden() 
 	return false 
-end
-
-function modifier_ancient_defense:IsPurgable() 
-	return false 
-end
-
-function modifier_ancient_defense:IsDebuff() 
-	return false 
-end
-
-function modifier_ancient_defense:GetTexture()
-    return "modifier_ancient_defense"
 end
 
 function modifier_ancient_defense:OnCreated(keys)
@@ -24,91 +13,74 @@ function modifier_ancient_defense:OnCreated(keys)
 	self:StartIntervalThink(0.1)
 end
 
-function modifier_ancient_defense:OnIntervalThink()
-	local friends = FindUnitsInRadius( 
-		self:GetParent():GetTeam(), 
-		self:GetParent():GetOrigin(), 
-		target,
-		2250, 
-		DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
-		DOTA_UNIT_TARGET_HERO, 
-		DOTA_UNIT_TARGET_FLAG_NO_INVIS, 
-		0, 
-		false 
-	)
+function modifier_ancient_defense:OnIntervalThink()	
+	local friends = nil
 
-	if #friends > 0 then
-		for _,friend in pairs(friends) do
-			if friend ~= nil and not friend:FindModifierByName("modifier_fountain_aura_buff") then
-				if self:GetParent():GetHealth() <= 500 then
-					self:GetParent():AddNewModifier(self:GetParent(), nil, "modifier_ancient_defense_effect", nil)
+	friends = self:FindFriends(2250)
+	if friends ~= nil then
+		friend = self:WorkFriends(friends)
+
+		if friend ~= nil then	
+			if friend:IsAlive() and self:GetParent():GetHealth() == 1 then
+				if not friend:FindModifierByName("modifier_fountain_invulnerability") then
+					self:PlayEffects( friend )
 				end
 			end
 		end
 	end
 
-	if self:GetParent():GetHealth() > 500 then
-		if self:GetParent():FindModifierByName("modifier_ancient_defense_effect") then
-			self:GetParent():RemoveModifierByName("modifier_ancient_defense_effect")
+	friends = self:FindFriends(9999)
+	if friends ~= nil then
+		friend = self:WorkFriends(friends)
+		
+		if friend ~= nil then	
+			if self:GetParent():GetHealth() > 1 or not friend:IsAlive() or friend:FindModifierByName("modifier_fountain_invulnerability") then
+				self:StopEffects( friend )
+			end
+
+			if not friend:FindModifierByName("modifier_ancient_defense_effect_hero") or not self:GetParent():FindModifierByName("modifier_ancient_defense_effect") then
+				self:StopEffects( friend )
+			end
 		end
 	end
 end -- oh yeeee if if ifi fifi eneneenednendne end
 
-function modifier_ancient_defense:OnAttackLanded(keys)
-	local attacker = keys.attacker
-	local target = keys.target
-	local damage = keys.damage
-
-	if self:GetParent():GetHealth() > 500 then return end
-	if self:GetParent():GetHealth() > damage then return end
-
+function modifier_ancient_defense:FindFriends(radius)
 	local friends = FindUnitsInRadius( 
 		self:GetParent():GetTeam(), 
 		self:GetParent():GetOrigin(), 
 		target,
-		2250, 
+		radius, 
 		DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
 		DOTA_UNIT_TARGET_HERO, 
-		DOTA_UNIT_TARGET_FLAG_NO_INVIS, 
+		DOTA_UNIT_TARGET_FLAG_NONE + DOTA_UNIT_TARGET_FLAG_DEAD, 
 		0, 
 		false 
 	)
+	
+	return friends
+end
 
-	if #friends > 0 then
-		for _,friend in pairs(friends) do
-			if friend ~= nil and not friend:FindModifierByName("modifier_fountain_aura_buff") then
-				local ancient_hp = self:GetParent():GetHealth()
-				local addinghp = ancient_hp + damage
-				self:GetParent():ModifyHealth(addinghp, self, false, 0)
-
-				self:PlayEffects( friend )
-
-				friend:AddNewModifier(friend, nil, "modifier_ancient_defense_bonus", {duration = 5})
-			end
+function modifier_ancient_defense:WorkFriends(friends)
+	for _,friend in pairs(friends) do
+		if friend ~= nil then	
+			return friend
 		end
 	end
-
+	return nil
 end
 
-function modifier_ancient_defense:DeclareFunctions()
-	local funcs = {
-		MODIFIER_EVENT_ON_ATTACK_LANDED
-	}
+function modifier_ancient_defense:PlayEffects(friend)
+	if friend.effect_cast_enabled then return end
 
-	return funcs
-end
-
-function modifier_ancient_defense:PlayEffects( friend )
-	local particle_cast = "particles/units/heroes/hero_tiny/tiny_tree_channel_tree_arc.vpcf"
-	
+	local particle_cast = "particles/econ/items/pugna/pugna_ti10_immortal/pugna_ti10_immortal_life_drain_gold_shard_beam_core.vpcf"
 	local entity = self:GetParent()
-	local direction = (entity:GetOrigin() - friend:GetOrigin()):Normalized()
 	
-	local effect_cast = ParticleManager:CreateParticle(particle_cast, PATTACH_ABSORIGIN, entity )
+	friend.effect_cast = ParticleManager:CreateParticle(particle_cast, PATTACH_ABSORIGIN, entity )
 	local attach = "attach_attack1"
 	if entity:ScriptLookupAttachment( "attach_attack2" ) ~= 0 then attach = "attach_attack2" end
 	ParticleManager:SetParticleControlEnt(
-		effect_cast,
+		friend.effect_cast,
 		0,
 		entity,
 		PATTACH_POINT_FOLLOW,
@@ -116,8 +88,13 @@ function modifier_ancient_defense:PlayEffects( friend )
 		Vector(0,0,0), 
 		true 
 	)
+
+	friend.effect_cast_enabled = true
+
+	local direction = (entity:GetOrigin() - friend:GetOrigin()):Normalized()
+
 	ParticleManager:SetParticleControlEnt(
-		effect_cast,
+		friend.effect_cast,
 		1,
 		friend,
 		PATTACH_POINT_FOLLOW,
@@ -125,10 +102,59 @@ function modifier_ancient_defense:PlayEffects( friend )
 		Vector(0,0,0), 
 		true 
 	)
-	ParticleManager:SetParticleControl( effect_cast, 2, friend:GetOrigin() )
-	ParticleManager:SetParticleControl( effect_cast, 3, friend:GetOrigin() + direction )
-	ParticleManager:SetParticleControlForward( effect_cast, 3, -direction )
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+	ParticleManager:SetParticleControl( friend.effect_cast, 2, friend:GetOrigin() )
+	ParticleManager:SetParticleControl( friend.effect_cast, 3, friend:GetOrigin() + direction )
+	ParticleManager:SetParticleControlForward( friend.effect_cast, 3, -direction )
+end
+
+function modifier_ancient_defense:StopEffects( friend )
+	if friend.effect_cast_enabled == true then
+		ParticleManager:DestroyParticle(friend.effect_cast, false)
+		ParticleManager:ReleaseParticleIndex(friend.effect_cast)
+		friend.effect_cast_enabled = false
+	end
+end
+
+function modifier_ancient_defense:IsAura() 					
+	return true 
+end
+function modifier_ancient_defense:IsAuraActiveOnDeath()		
+	return false 
+end
+function modifier_ancient_defense:GetAuraRadius()			
+	return 2250 
+end
+function modifier_ancient_defense:GetAuraSearchFlags()		
+	return DOTA_UNIT_TARGET_FLAG_NONE 
+end
+function modifier_ancient_defense:GetAuraSearchTeam()		
+	return DOTA_UNIT_TARGET_TEAM_FRIENDLY 
+end
+function modifier_ancient_defense:GetAuraSearchType()		
+	return DOTA_UNIT_TARGET_HERO
+end
+function modifier_ancient_defense:GetModifierAura()			
+	return "modifier_ancient_defense_effect_hero" 
+end
+
+-------------------------------------------------------------------------------------
+
+modifier_ancient_defense_effect_hero = class({})
+
+function modifier_ancient_defense_effect_hero:IsHidden()
+	return false
+end
+
+function modifier_ancient_defense_effect_hero:IsPurgable() 
+	return false 
+end
+
+function modifier_ancient_defense_effect_hero:IsDebuff() 
+	return false 
+end
+
+function modifier_ancient_defense_effect_hero:GetTexture()
+    return "modifier_ancient_defense"
 end
 
 -------------------------------------------------------------------------------------
@@ -136,15 +162,31 @@ end
 modifier_ancient_defense_effect = class({})
 
 function modifier_ancient_defense_effect:IsHidden()
-	return true
+	return false
 end
 
-function modifier_ancient_defense_effect:GetEffectName()
-    return "particles/dev/library/base_overhead_follow.vpcf"
+function modifier_ancient_defense_effect:IsPurgable() 
+	return false 
 end
 
-function modifier_ancient_defense_effect:GetEffectAttachType()
-    return PATTACH_OVERHEAD_FOLLOW
+function modifier_ancient_defense_effect:IsDebuff() 
+	return false 
+end
+
+function modifier_ancient_defense_effect:GetTexture()
+    return "modifier_ancient_defense"
+end
+
+function modifier_ancient_defense_effect:CheckState()
+	return {
+		[MODIFIER_STATE_INVULNERABLE] = true
+	}
+end
+
+function modifier_ancient_defense_effect:OnDestroy() 
+	if not IsServer() then return end
+
+	self:GetParent():SetBaseHealthRegen(12.0)
 end
 
 -------------------------------------------------------------------------------------
